0312249a2b9b3044f08d1443a8203b507f9857bb
Vawser <ben.rouncefield@gmail.com>
2018-10-05 11:01:47 +0100

    Commit

---
+++ common/defines/00_ndx.lua
@@ -0,0 +1,13 @@
+NDefines.NNationDesigner.IDEAS_MAX_LEVEL = 10
+
+NDefines.NNationDesigner.IDEAS_LEVEL_COST_1 = 0
+NDefines.NNationDesigner.IDEAS_LEVEL_COST_2 = 5
+NDefines.NNationDesigner.IDEAS_LEVEL_COST_3 = 15
+NDefines.NNationDesigner.IDEAS_LEVEL_COST_4 = 30
+NDefines.NNationDesigner.IDEAS_LEVEL_COST_5 = 50
+NDefines.NNationDesigner.IDEAS_LEVEL_COST_6 = 75
+NDefines.NNationDesigner.IDEAS_LEVEL_COST_7 = 90
+NDefines.NNationDesigner.IDEAS_LEVEL_COST_8 = 100
+NDefines.NNationDesigner.IDEAS_LEVEL_COST_9 = 110
+NDefines.NNationDesigner.IDEAS_LEVEL_COST_10 = 120
+
---
+++ ndx_balanced.mod
@@ -0,0 +1,8 @@
+name="Nation Designer Extender"
+path="mod/ndx_balanced"
+replace_path="common/custom_ideas"
+tags={
+	"Gameplay"
+}
+picture="image.png"
+supported_version="1.27.*.*"
